bf0f72306e4638fc332bfbf9aed8e3cd0d3de192
Wojciech Kowal <w.kowal202@gmail.com>
2025-10-21 19:44:40 +0200

    fix: deleted the wrong file ...

---
+++ wroclawskie-szamba-medusa/medusa-config.ts
@@ -0,0 +1,51 @@
+import { loadEnv, defineConfig } from '@medusajs/framework/utils';
+
+loadEnv(process.env.NODE_ENV || 'development', process.cwd());
+
+module.exports = defineConfig({
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
+    workerMode:
+      (process.env.WORKER_MODE as 'shared' | 'worker' | 'server') || 'shared',
+    http: {
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      authCors: process.env.AUTH_CORS!,
+      jwtSecret: process.env.JWT_SECRET || 'supersecret',
+      cookieSecret: process.env.COOKIE_SECRET || 'supersecret',
+    },
+    databaseDriverOptions:
+      process.env.NODE_ENV === 'production'
+        ? {
+            connection: {
+              ssl: { rejectUnauthorized: false },
+            },
+          }
+        : { ssl: false, sslmode: 'disable' },
+
+    cookieOptions: {
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+      secure: process.env.NODE_ENV === 'production' ? true : false,
+    },
+  },
+  modules: [
+    {
+      resolve: '@medusajs/medusa/file',
+      options: {
+        providers: [
+          {
+            resolve: '@medusajs/medusa/file-local',
+            id: 'local',
+            options: {
+              upload_dir: 'static',
+              backend_url: process.env.BACKEND_URL
+                ? `${process.env.BACKEND_URL}/static`
+                : 'http://localhost:9000/static',
+            },
+          },
+        ],
+      },
+    },
+  ],
+});
